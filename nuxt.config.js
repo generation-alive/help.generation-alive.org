1b35d31ddde8ed9f6c14753e5b8b9fa1e371660e
Marvin Klimm <git@marvinklimm.de>
2021-04-09 12:29:44 +0200

    🔧 changed from en to de locale

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,5 +3,14 @@ import theme from '@nuxt/content-theme-docs'
 export default theme({
   docs: {
     primaryColor: '#E24F55'
+  },
+  i18n: {
+    locales: () => [{
+      code: 'de',
+      iso: 'de-DE',
+      file: 'de-DE.js',
+      name: 'Deutsch'
+    }],
+    defaultLocale: 'de'
   }
 })
