aa99ae5100a09d490af41088c017fabd238fc642
Marvin Klimm <git@marvinklimm.de>
2021-04-16 17:28:27 +0200

    💬 introduced legal links

---
+++ components/global/docus/DMadeWithDocus.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="p-3 text-center border-t border-gray-200 border-dashed dark:border-gray-800 dark:bg-gray-900">
+    <a href="https://generation-alive.org" target="_blank" class="text-xs text-gray-400 hover:text-gray-600 dark:text-gray-600 group dark:hover:text-gray-400">
+      <strong class="font-bold">&copy; 2021 Generation Alive e.V.</strong>
+    </a>
+    <a href="https://www.generation-alive.org/impressum" target="_blank" class="mx-2 text-xs text-gray-400 hover:text-gray-600 dark:text-gray-600 group dark:hover:text-gray-400">
+      Impressum
+    </a>
+    <a href="https://www.generation-alive.org/datenschutzbestimmungen" target="_blank" class="text-xs text-gray-400 hover:text-gray-600 dark:text-gray-600 group dark:hover:text-gray-400">
+      Datenschutzbestimmungen
+    </a>
+  </div>
+</template>
